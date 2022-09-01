10823a5ce3e47dff1ca2a12463792278488d915d
tenmihi <tenmihi@gmail.com>
2022-09-01 22:52:27 +0900

    chapter-6: refactor error handlers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,7 @@ class ApplicationController < ActionController::Base
 
   class Forbidden < ActionController::ActionControllerError; end
   class IpAddressRejected < ActionController::ActionControllerError; end
-
-  rescue_from StandardError, with: :rescue500
-  rescue_from Forbidden, with: :rescue403
-  rescue_from IpAddressRejected, with: :rescue403
+  include ErrorHandlers if Rails.env.production?
   
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)})
@@ -14,14 +11,5 @@ class ApplicationController < ActionController::Base
     else
       "customer"
     end
-  end
-
-  private def rescue403(e)
-    @exception = e
-    render "errors/forbidden", status: 403
-  end
-
-  private def rescue500(e)
-    render "errors/internal_server_error", status: 500
   end
 end
